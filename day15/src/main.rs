e16b8d8b667481d8ff7ad76a461e4f1f555b58a0
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-15 14:10:21 +0100

    Add file reading

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("./input")
+        .expect("Should have been able to read the file");
+
+    let mut split = contents.split("\n\n");
+
+    let board_part = split.next().unwrap();
+    let moves_part = split.next().unwrap();
+
+    print!("{}", board_part);
+    print!("{}", moves_part);
 }
